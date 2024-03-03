9f4a2db730928929b82007d555fd8007b59ba468
AdilAyub12 <160512251+AdilAyub12@users.noreply.github.com>
2024-03-03 13:03:08 +0500

    Add files via upload

---
+++ aap.ts
@@ -0,0 +1,18 @@
+// let personName ="Eric";
+// console.log("hello",personName,"Would you like to learn some python today?");
+//Exercise 3
+// let m1:string="Abdul"
+// console.log(m1.toLowerCase());
+// let m2:string="Areesha";
+// console.log(m2.toUpperCase());
+// let m3:string="maaz";
+// console.log(m3.charAt(0).toUpperCase()+ m3.slice(1,4));
+
+// Exercise 4 famous qoute 
+// console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new."');
+// Exercise #5 famous qoute in a variable
+// const m4:string="areesha"
+// console.log(m4.charAt(0).toUpperCase()+m4.slice(1,6));
+
+// let famous_person:string=('Albert Einstein once said, “A person who never made a mistake never tried anything new."');
+// console.log(famous_person);
